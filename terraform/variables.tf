c45921c8f7685c12482e9c821e2d0a91b836d68b
John-Paul Drawneek <jpdrawneek@users.noreply.github.com>
2020-11-03 09:30:06 +0000

    Add some validation

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,9 +9,17 @@ variable "subdomain" {
 
 variable "environment" {
   type = string
+  validation {
+    condition     = contains(["dev", "test", "stage", "prod"], var.environment)
+    error_message = "Environment vaild values are: dev, test, stage, prod."
+  }
 }
 
 variable "price_class" {
   type = string
   default = "PriceClass_100"
+  validation {
+    condition     = contains(["PriceClass_100", "PriceClass_200", "PriceClass_All"], var.price_class)
+    error_message = "Price Class value not valid, check docs."
+  }
 }
